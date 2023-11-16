be295c3e6f72fb38c308e4bc9898744fb4aeff3f
Alchemist <82286271+alchemistteam@users.noreply.github.com>
2023-11-16 03:21:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { OpenAI } = require("openai");
-require("dotenv").config();
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
